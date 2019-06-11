fb1e865ea8372c4aea519955265876575165f99a
irfanwalele <irfanwalele@gmail.com>
2019-06-10 20:09:13 -0400

    api info added

--- boredboard.js
+++ boredboard.js
@@ -1,42 +1,38 @@
-// Maybe use the Eventful API? https://rapidapi.com/volodimir.kudriachenko/api/Eventful
 // Congratulations! Your new application key is fcb8bhZMVpHTfWJV.
 // https://api.eventful.com/docs
 // https://api.eventful.com/libs/javascript/
 
-<html>
-Javascript Sample page <br>
-<script type="text/javascript" src="http://api.eventful.com/js/api">
-</script>
-<script type="text/javascript">
-function show_alert()
-{
-  var oArgs = {
-            app_key:"fcb8bhZMVpHTfWJV",
-            id: "20218701",
-            page_size: 25 ,
-  };
-  EVDB.API.call("/events/get", oArgs, function(oData) {
-      // Note: this relies on the custom toString() methods below
-    });
+function buildQueryURL() {
+    // queryURL is the url we will use to search the API
+    var queryURL = "http://api.eventful.com/json/events/?q=music&l=10018&within10&units=miles&app_key=fcb8bhZMVpHTfWJV"
+
+    // here is the application key
+
+    // http://api.eventful.com/rest/events/search?...&keywords=books&location=San+Diego&date=Future
+
+
 }
-function show_alert2()
-{
-   var oArgs = {
-      app_key: "fcb8bhZMVpHTfWJV",
-      q: "music",
-      where: "San Diego", 
-      "date": "2013061000-2015062000",
-      page_size: 5,
-      sort_order: "popularity",
-   };
-   EVDB.API.call("/events/search", oArgs, function(oData) {
-      // Note: this relies on the custom toString() methods below
-    });
-}
-</script>
-<body>
-Run Sample api searches <br><br>
-<input type="button" onclick="show_alert()" value="Query #1 " /> <br>
-<input type="button" onclick="show_alert2()" value="Query #2 " /> <br>
-</body>
-</html>
+
+var what = $("#what")
+
+var where = $("#where")
+
+var radius = $("#radius")
+
+var queryURL = "http://api.eventful.com/json/events/search?keywords="+ what +"&l="+ where +"&within"+ radius +"&units=miles&app_key=fcb8bhZMVpHTfWJV"
+
+
+
+$.ajax({
+    url:queryURL,
+    method: "GET",
+    dataType:'jsonp'
+}) .then(function (response){
+    console.log(response)
+        
+    for (var i = 0; i < response; i++){
+        console.log(response.events.event[i])
+    }
+});
+
+console.log("123123123")
